84a41a4a3be7f347697d35e2504c8320baf09965
Axepapag <axemanjeffro@gmail.com>
2026-09-21 14:19:22 -0500

    Switch local app to VM control center

--- android/app/src/local/java/com/gliksbot/axonhome/MainActivity.kt
+++ android/app/src/local/java/com/gliksbot/axonhome/MainActivity.kt
@@ -3,11 +3,10 @@ package com.gliksbot.axonhome
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.gliksbot.axonhome.phone.PhoneHome
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { PhoneHome() }
+        setContent { RemoteHome() }
     }
 }
